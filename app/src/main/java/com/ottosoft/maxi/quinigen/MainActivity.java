caeeb6abe5bdc10173faac58aa4857133eaa80aa
Maxi <maxi.ottino@gmail.com>
2017-07-25 19:59:28 -0300

    Listener para la seleccion de la jugada con checkbox

--- app/src/main/java/com/ottosoft/maxi/quinigen/MainActivity.java
+++ app/src/main/java/com/ottosoft/maxi/quinigen/MainActivity.java
@@ -8,14 +8,19 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.Interpolator;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     EditText edtTxt;
     Button btnPrincipal;
+
+    CheckBox jugada1 , jugada2 , jugada3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +31,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         edtTxt = (EditText) findViewById(R.id.txtNumAleatorio);
         btnPrincipal = (Button) findViewById(R.id.btnPrincipal);
 
+        jugada1 = (CheckBox) findViewById(R.id.checkBox_jugada1);
+        jugada2 = (CheckBox) findViewById(R.id.checkBox_jugada2);
+        jugada3 = (CheckBox) findViewById(R.id.checkBox_jugada3);
+
+
         btnPrincipal.setOnClickListener(this);
+        jugada1.setOnClickListener(this);
+        jugada2.setOnClickListener(this);
+        jugada3.setOnClickListener(this);
+
     }
 
     @Override
@@ -54,9 +68,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
 
+        Random numeroSuerte = new Random();
+
         switch (v.getId()) {
+
             case R.id.btnPrincipal:
-                edtTxt.setText("Tocaste el boton");
+
+                edtTxt.setText("Tú número de la suerte es: " + Integer.toString(numeroSuerte.nextInt(9999)));
+
+                break;
+
+            case R.id.checkBox_jugada1:
+
+                jugada2.setChecked(false);
+                jugada3.setChecked(false);
+
+                break;
+
+            case R.id.checkBox_jugada2:
+
+                jugada1.setChecked(false);
+                jugada3.setChecked(false);
+
+                break;
+
+            case R.id.checkBox_jugada3:
+
+                jugada1.setChecked(false);
+                jugada2.setChecked(false);
+
                 break;
         }
 
